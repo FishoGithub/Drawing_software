5c07cd2720a6bff63589985003fd2d5dc45ed4f9
MihirMagikarp <mihir.majgaonkar@gmail.com>
2020-10-31 01:54:39 +0000

    added many colors, and added an in-progress eraser tool.

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 var canvas = document.getElementById('myCanvas');
 var clear = document.getElementById('clear');
+var blue = document.getElementById('blue');
+var black = document.getElementById('black');
+var red = document.getElementById('red');
+var purple = document.getElementById('purple');
+var context = canvas.getContext('2d');
+var eraserContext = canvas.getContext('2d');
+
+black.style.background = 'black';
+blue.style.background = 'blue';
+red.style.background = 'red';
+purple.style.background = 'purple';
+yellow.style.background = 'yellow';
+orange.style.background = 'orange';
+pink.style.background = 'pink';
 
 canvas.width = 500;
 canvas.height = 500;
 
-var context = canvas.getContext('2d');
-var mouseDown = false;
-
-context.fillStyle = 'red';
+context.fillStyle = 'black';
 context.strokeStyle = 'purple';
 
 function drawHouse() {
@@ -28,6 +39,39 @@ canvas.onmousedown = function(e) {
 
   console.log('x coordinate is ' + x + '.');
   console.log('y coordinate is ' + y + '.');
+}
+
+function eraserSize() {
+  context.fillRect(context.pageX, context.pageY, 200, 200);
+}
+
+// colors:
+function blueColor() {
+  context.fillStyle = 'blue';
+}
+function blackColor() {
+  context.fillStyle = 'black';
+}
+function redColor() {
+  context.fillStyle = 'red';
+}
+function purpleColor() {
+  context.fillStyle = 'purple';
+}
+function yellowColor() {
+  context.fillStyle = 'yellow';
+}
+function orangeColor() {
+  context.fillStyle = 'orange';
+}
+function pinkColor() {
+  context.fillStyle = 'pink';
+}
+
+function eraser() {
+  
+  eraserContext.fillStyle = 'white';
+  eraserContext.fillRect(eraserContext.pageX -20, eraserContext.pageY -20, 1000, 1000);
 }
 
 function clearScreen() {
